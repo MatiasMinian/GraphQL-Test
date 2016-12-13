9f7f9117b76a86792336ab5d64e90eb99ca5952d
Matias Mariano Minian <matias.minian@globant.com>
2016-12-13 14:50:18 -0300

    Create project

---
+++ src/main/java/com/softsign/graph-toro-test/GraphToroDemoApplication.java
@@ -0,0 +1,12 @@
+package com.softsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphToroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphToroDemoApplication.class, args);
+	}
+}
